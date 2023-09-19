ededa958a6ce51bc9e6a764921570bd3b11a3266
Ratatouille <cshimmaincontact@gmail.com>
2023-09-19 13:43:38 -0400

    Added Functional Code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "a1",
+    title: "Car Insurance",
+    amount: 295.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "a2",
+    title: "Toilet Paper",
+    amount: 10.23,
+    date: new Date(2021, 5, 14),
+  },
+  { id: "a3", title: "Desk", amount: 100.99, date: new Date(2020, 2, 1) },
+  { id: "a4", title: "Chair", amount: 34.55, date: new Date(2020, 2, 21) },
+];
 
 function App() {
-  const expenses = [
-    { title: "Car Insurance", amount: 295.67, date: new Date(2001, 2, 28) },
-    { title: "Toilet Paper", amount: 10.23, date: new Date(2003, 5, 14) },
-    { title: "Desk", amount: 100.99, date: new Date(2011, 2, 1) },
-    { title: "Chair", amount: 34.55, date: new Date(2011, 2, 21) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div className="App">
       <h2>Stat Tracker</h2>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
